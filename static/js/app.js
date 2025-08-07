28704109bb30a063c20ebb0999ac92527003d74d
koepbkk <168929025+koepbkk@users.noreply.github.com>
2025-08-07 20:34:22 +0700

    Initial commit: project skeleton

---
+++ static/js/app.js
@@ -0,0 +1,8 @@
+
+### 4. Commit to Git and Integrate with GitHub
+
+With the initial files created, let's make our first commit.
+
+```bash
+git add .
+git commit -m "Initial commit: project skeleton"
